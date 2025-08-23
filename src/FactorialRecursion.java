45de03fb06033e3f34727d933f335e542c383d92
Bongani X <velanibzuma@gmail.com>
2025-08-23 19:37:27 +0200

    -edge cases for negative and 0 added

--- src/FactorialRecursion.java
+++ src/FactorialRecursion.java
@@ -1,8 +1,12 @@
 public class FactorialRecursion{
 
     static int factorial(int num){
-        if(num == 0){
+
+        if(num - 1 == 0){
             return 1;
+        }else if(num == 0 || num < 0){
+            System.out.println("Consider numbers greater than 1!");
+            return 0;
         }
         
         return num * factorial(num - 1);
@@ -10,7 +14,7 @@ public class FactorialRecursion{
     }
 
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(factorial(-399));
 
     }
 }
