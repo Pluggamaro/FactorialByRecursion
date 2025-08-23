3efa17840d2ad8700c5cb376d16c45f7119d520a
Bongani X <velanibzuma@gmail.com>
2025-08-23 18:58:17 +0200

    added factorial recursion method, missing edge case guide

--- src/FactorialRecursion.java
+++ src/FactorialRecursion.java
@@ -1,5 +1,16 @@
 public class FactorialRecursion{
-    public static void main(String[] args) {
+
+    static int factorial(int num){
+        if(num == 0){
+            return 1;
+        }
         
+        return num * factorial(num - 1);
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+
     }
 }
